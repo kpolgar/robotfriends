893eba38dc7fff6ae2e2d31561b0f5f311863deb
Kinga Polgar <kpolgar27@gmail.com>
2019-07-30 08:46:59 -0400

    Updates

---
+++ precache-manifest.e267e4fed6afa43e6d2b9d4a5aac94eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda422921e633fbd789d05b1bcbf933a",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robotfriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "35ffa780b18331760eee",
+    "url": "/robotfriends/static/css/main.7bdfd682.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robotfriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "35ffa780b18331760eee",
+    "url": "/robotfriends/static/js/main.b6c82b0a.chunk.js"
+  },
+  {
+    "revision": "aebb4e0e6c2039467182",
+    "url": "/robotfriends/static/js/runtime~main.6fe61ee8.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
